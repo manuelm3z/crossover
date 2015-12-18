58a0783f0cdfc7980678f6f2d040629c4a785432
Manuel Zambrano <mmzmm3z@gmail.com>
2015-12-17 23:55:09 -0430

    actualizar y cerrar graficos

--- Code Folder/source/js/controllers/DashboardController.js
+++ Code Folder/source/js/controllers/DashboardController.js
@@ -1,44 +1,69 @@
 app.controller('DashboardController', ['$rootScope', '$scope', 'SalesMan', 'SalesPerMonth', 'TopSalesOrders', 'TopSalesmen', function ($rootScope, $scope, SalesMan, SalesPerMonth, TopSalesOrders, TopSalesmen) {
-	$scope.dataSalesman = [];
-    $scope.labelsSalesman = [];
+	$scope.showTotalSalesman = true;
+	$scope.totalSalesman = function () {
+	    $scope.dataSalesman = [];
+        $scope.labelsSalesman = [];
 
-	SalesMan.query(function (data) {
-		if (data.resultDescription === "SUCCESS") {
-			for (var i in data.data) {
-				if (data.data[i] !== undefined) {
-					$scope.dataSalesman.push(data.data[i][1]); 
-					$scope.labelsSalesman.push(data.data[i][0]);
-				}
-			}
-		}
-	});
-    $scope.dataPerMonth = Array([]);
-    $scope.labelsPerMonth = [];
+	    SalesMan.query(function (data) {
+		    if (data.resultDescription === "SUCCESS") {
+			    for (var i in data.data) {
+				    if (data.data[i] !== undefined) {
+					    $scope.dataSalesman.push(data.data[i][1]); 
+					    $scope.labelsSalesman.push(data.data[i][0]);
+				    }
+			    }
+		    }
+	    });
+	};
+	$scope.totalSalesman();
 
-	SalesPerMonth.query(function (data) {
-		if (data.resultDescription === "SUCCESS") {
-			for (var i in data.data) {
-				if (data.data[i] !== undefined) {
-					$scope.dataPerMonth[0].push(data.data[i][1]); 
-					$scope.labelsPerMonth.push(data.data[i][0]);
-				}
-			}
-		}
-	});
+    $scope.showTotalSalesPerMonth = true;
+	$scope.totalSalesPerMonth = function () {
+		$scope.dataPerMonth = Array([]);
+        $scope.labelsPerMonth = [];
 
-    $scope.orders = [];
+	    SalesPerMonth.query(function (data) {
+		    if (data.resultDescription === "SUCCESS") {
+			    for (var i in data.data) {
+				    if (data.data[i] !== undefined) {
+					    $scope.dataPerMonth[0].push(data.data[i][1]); 
+					    $scope.labelsPerMonth.push(data.data[i][0]);
+				    }
+			    }
+		    }
+	    });
+	};
+	$scope.totalSalesPerMonth();
 
-	TopSalesOrders.query(function (data) {
-		if (data.resultDescription === "SUCCESS") {
-			$scope.orders = data.data;
-		}
-	});
+    $scope.showTopSalesOrders = true;
+	$scope.topSalesOrders = function () {
+		$scope.orders = [];
 
-	$scope.salesmen = [];
+	    TopSalesOrders.query(function (data) {
+		    if (data.resultDescription === "SUCCESS") {
+			    $scope.orders = data.data;
+		    }
+	    });
+	};
+	$scope.topSalesOrders();
 
-	TopSalesmen.query(function (data) {
-		if (data.resultDescription === "SUCCESS") {
-			$scope.salesmen = data.data;
-		}
-	});
+    $scope.showTopSalesmen = true;
+    $scope.topSalesmen = function () {
+	    $scope.salesmen = [];
+
+	    TopSalesmen.query(function (data) {
+		    if (data.resultDescription === "SUCCESS") {
+			    $scope.salesmen = data.data;
+		    }
+	    });
+    };
+    $scope.topSalesmen();
+
+	$scope.update = function (name) {
+		$scope[name]();
+	};
+
+	$scope.close = function (name) {
+		$scope[name] = false;
+	};
 }]);
